04797a9f970aaede422ac92b28ef0d88417585d5
Chris Marinas <chris.marinas@gmail.com>
2016-11-11 13:04:07 -0600

    trying to understand how relative paths work when dealing with packages

--- bin/register.js
+++ bin/register.js
@@ -4,13 +4,14 @@
 var path = require('path'),
     argv = require('minimist')(process.argv.slice(2)),
     usage = require('cli-usage'),
-    shell = require('shelljs');
+    shell = require('shelljs'),
+    chalk = require('chalk');
 
 usage('../docs/usage.md');
 
 if (argv.v || argv.version) {
   var pkg = require(path.resolve(__dirname, '../package.json'));
-  console.log('Dashboard Module Registration v' + pkg.version);
+  console.log(chalk.green('Dashboard Module Registration') + chalk.white(' v' + pkg.version));
   process.exit(0);
 }
 
@@ -19,7 +20,13 @@ var dashModule = argv._[0];
 // install package
 if (typeof dashModule !== 'undefined') {
   shell.exec('npm install ' + dashModule);
+} else {
+  console.log(chalk.white('No module to install.'));
 }
+
+let relativePath = path.relative(__dirname, process.cwd());
+
+console.log(relativePath);
 
 // generate inventory file, if it doesn't exist
 
